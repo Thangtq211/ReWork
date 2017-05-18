1c87bfef6aa79c9647c5faca56aa6308f9be3046
thangtq <thangtq211@gmail.com>
2017-05-18 14:32:14 +0700

    passed with a functional test which is interacting weblogic

--- authenticateb/trunk/src/main/java/com/tmobile/federation/dao/IAMUserDAOImpl.java
+++ authenticateb/trunk/src/main/java/com/tmobile/federation/dao/IAMUserDAOImpl.java
@@ -1,60 +1,55 @@
 package com.tmobile.federation.dao;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Types;
-import java.util.logging.Logger;
-
 import com.tmobile.federation.beans.req.FedIdRequestBean;
-import com.tmobile.wsg.services.WSGCDBProfileService;
-import org.springframework.stereotype.Component;
+import com.tmobile.federation.config.DatabaseConfig;
 import org.springframework.stereotype.Service;
 
-import com.tmobile.federation.config.DatabaseConfig;
-
+import java.sql.*;
 
 /**
- * 
+ *
  * @author Admin
  *
  *
  * @since 1.0
  */
 @Service
-public class IAMUserDAOImpl implements IAMUserDAO{
-	private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(IAMUserDAOImpl.class);
-	private Connection databaseConnection;
-	
-	public IAMUserDAOImpl() {
-		logger.debug("Constructor");
-		this.databaseConnection = DatabaseConfig.getDataConnection();
-	}
+public class IAMUserDAOImpl implements IAMUserDAO {
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(IAMUserDAOImpl.class);
+    private Connection databaseConnection;
 
-	@Override
-	public String createFederatedId(String email, String userId, String tenantID, String createdBy) {
-		try {
-			if(DatabaseConfig.getDataConnection() == null){
-				logger.error("Database connection not found");
-			}
-			CallableStatement callableStatement = databaseConnection.prepareCall("{call fbi_stg_tenant_user_chk(?,?,?,?,?)}");
-			callableStatement.setString(1, userId);
-			callableStatement.setString(2, tenantID);
-			callableStatement.setString(3, email);
-			callableStatement.setString(4, createdBy);
-			callableStatement.registerOutParameter(5, Types.VARCHAR);
-			callableStatement.executeUpdate();
-			closeDataBaseConnection();
-			String result = callableStatement.getString(5);
-			return result;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			closeDataBaseConnection();
-		}
-		return null;
-	}
+    public IAMUserDAOImpl() {
+        this.databaseConnection = DatabaseConfig.getDataConnection();
+    }
 
+    @Override
+    public String createFederatedId(String email, String userId, String tenantID, String createdBy) {
+        CallableStatement callableStatement = null;
+        try {
+            if (DatabaseConfig.getDataConnection() == null) {
+                logger.error("Database connection not found");
+            }
+            checkAndReTryConnection();
+            callableStatement = databaseConnection.prepareCall("{call fbi_stg_tenant_user_chk(?,?,?,?,?)}");
+            callableStatement.setString(1, userId);
+            callableStatement.setString(2, tenantID);
+            callableStatement.setString(3, email);
+            callableStatement.setString(4, createdBy);
+            callableStatement.registerOutParameter(5, Types.VARCHAR);
+            callableStatement.executeUpdate();
+            String result = callableStatement.getString(5);
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (callableStatement != null) callableStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
     /**
      * @param iamUserId
      * @param email
@@ -63,12 +58,14 @@ public class IAMUserDAOImpl implements IAMUserDAO{
      */
     @Override
     public String getFederatedId(String iamUserId, String email, String tenantId) {
+        CallableStatement callableStatement = null;
         try {
-        	logger.debug("getting FederatedId with iamUserId:"+iamUserId);
-            if(DatabaseConfig.getDataConnection() == null){
-				logger.error("Database connection not found");
+            logger.debug("getting FederatedId with iamUserId:" + iamUserId);
+            if (DatabaseConfig.getDataConnection() == null) {
+                logger.error("Database connection not found");
             }
-            CallableStatement callableStatement = databaseConnection.prepareCall("{call fbi_stg_user_fed_id(?,?,?,?)}");
+            checkAndReTryConnection();
+            callableStatement = databaseConnection.prepareCall("{call fbi_stg_user_fed_id(?,?,?,?)}");
             callableStatement.setString(1, iamUserId);
             callableStatement.setString(2, email);
             callableStatement.setString(3, tenantId);
@@ -77,60 +74,82 @@ public class IAMUserDAOImpl implements IAMUserDAO{
             String result = callableStatement.getString(4);
             return result;
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
-            closeDataBaseConnection();
+        } finally {
+            try {
+                if (callableStatement != null) callableStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
 
+    @Override
+    public String getTenantRealmAndStatus(FedIdRequestBean response) {
+        String tenantRealm = "";
+        CallableStatement callableStatement = null;
 
-	@Override
-	public String getTenantRealmAndStatus(FedIdRequestBean response)
-	{
-		String tenantRealm="";
+        try {
+            logger.debug(" with response:" + response);
+            if (DatabaseConfig.getDataConnection() == null) {
+                logger.error("Database connection not found");
+            }
+            checkAndReTryConnection();
+            callableStatement = databaseConnection.prepareCall("{call FBI_GET_STATUS_TENANT_REALM(?,?,?,?,?)}");
+            callableStatement.setString(1, response.getIamUserId());
+            callableStatement.setString(2, response.getEmail());
+            //for STATUS
+            callableStatement.registerOutParameter(3, Types.VARCHAR);
+            //for tenant realm
+            callableStatement.registerOutParameter(4, Types.VARCHAR);
+            //for error message from the db
+            callableStatement.registerOutParameter(5, Types.VARCHAR);
+            callableStatement.executeUpdate();
+            response.setStatus(callableStatement.getString(3));
+            tenantRealm = callableStatement.getString(4);
+            String message = callableStatement.getString(5);
+            if (!message.contains("SUCCESS")) {
+                logger.error(message);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        } finally {
+            try {
+                if (callableStatement != null) callableStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
-		try {
-			logger.debug(" with response:"+response);
-			if(DatabaseConfig.getDataConnection() == null){
-				logger.error("Database connection not found");
-			}
-			CallableStatement callableStatement = databaseConnection.prepareCall("{call FBI_GET_STATUS_TENANT_REALM(?,?,?,?,?)}");
-			callableStatement.setString(1, response.getIamUserId());
-			callableStatement.setString(2, response.getEmail());
-			//for STATUS
-			callableStatement.registerOutParameter(3, Types.VARCHAR);
-			//for tenant realm
-			callableStatement.registerOutParameter(4, Types.VARCHAR);
-			//for error message from the db
-			callableStatement.registerOutParameter(5, Types.VARCHAR);
-			callableStatement.executeUpdate();
-			response.setStatus(callableStatement.getString(3));
-			tenantRealm = callableStatement.getString(4);
-			String message = callableStatement.getString(5);
-			closeDataBaseConnection();
-			if(!message.contains("SUCCESS")) {
-				logger.error(message);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			closeDataBaseConnection();
-			logger.error(e.getMessage());
-		}
-
-		return tenantRealm;
-	}
+        return tenantRealm;
+    }
 
 
-    private void closeDataBaseConnection(){
-		if(databaseConnection != null){
-			try{
-				databaseConnection.close();
-			}catch (Exception e) {
-				// TODO: handle exception
-			}
-		}
-	}
+
+    private void checkAndReTryConnection() {
+        boolean isValidConnection = false;
+        if (databaseConnection != null) {
+            Statement statement = null;
+            ResultSet rs = null;
+            try {
+                statement = databaseConnection.createStatement();
+                rs = statement.executeQuery("SELECT SYSDATE FROM DUAL");
+                while (rs.next()) isValidConnection = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    rs.close();
+                    statement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(!isValidConnection)
+            this.databaseConnection = DatabaseConfig.getDataConnection();
+    }
 
 }
--- authenticateb/trunk/src/test/java/com/tmobile/federation/services/CreateFedIdServiceTest.java
+++ authenticateb/trunk/src/test/java/com/tmobile/federation/services/CreateFedIdServiceTest.java
@@ -1,5 +1,9 @@
 package com.tmobile.federation.services;
 
+import com.tmobile.federation.beans.req.FedIdRequestBean;
+import com.tmobile.federation.beans.req.userProfile.UserProfiles;
+import com.tmobile.federation.beans.res.GlobalErrorBean;
+import com.tmobile.federation.beans.res.GlobalResponseBean;
 import com.tmobile.federation.dao.IAMUserDAO;
 import com.tmobile.federation.dao.IAMUserDAOImpl;
 import org.junit.Before;
@@ -9,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,10 +22,15 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.inject.Inject;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -61,4 +71,21 @@ public class CreateFedIdServiceTest {
         resultActions.andDo(MockMvcResultHandlers.print());
 
     }
+
+    @Test
+    public void queryFederatedIDFromWebLogic() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&email=email@mobile.com&tenantId=xyz123";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
 }
