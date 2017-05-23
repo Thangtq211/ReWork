266ec925b4caa31564bb9f00966393d84a6d8757
thangtq <thangtq211@gmail.com>
2017-05-23 17:11:59 +0700

    adding some tests with client

--- authenticateb/trunk/src/main/java/com/tmobile/federation/beans/validators/FedIdRequestValidator.java
+++ authenticateb/trunk/src/main/java/com/tmobile/federation/beans/validators/FedIdRequestValidator.java
@@ -1,54 +1,65 @@
 package com.tmobile.federation.beans.validators;
 
-
+import com.tmobile.federation.beans.req.FedIdRequestBean;
 import org.apache.commons.validator.routines.EmailValidator;
 
-import com.tmobile.federation.beans.req.FedIdRequestBean;
-
 /**
- * 
+ *
  * @author Admin
  *
  */
 
 public class FedIdRequestValidator {
 
-	
-	/**
-	 * 
-	 * @param fedIdRequestBean
-	 * 
-	 * Validate FedIdRequestBean
-	 * email, tenantId, status and role are mandatory fields
-	 * 
-	 * @return
-	 */
-	public static String validateFedIdRequestBean(FedIdRequestBean fedIdRequestBean){
-		if(fedIdRequestBean.getEmail() == null || fedIdRequestBean.getEmail().trim().isEmpty()){
-			return "email";
-		}else if(!EmailValidator.getInstance().isValid(fedIdRequestBean.getEmail())){
-			return "email";
-		}
-		if(fedIdRequestBean.getTenantId() == null || fedIdRequestBean.getTenantId().trim().isEmpty()){
-			return "tenantId";
-		}
-		if(fedIdRequestBean.getStatus() == null || fedIdRequestBean.getStatus().trim().isEmpty()){
-			return "status";
-		}
-		if(fedIdRequestBean.getRole() == null || fedIdRequestBean.getRole().trim().isEmpty()){
-		 return "role";
-		}
-		return null;
-	}
-
-
-    public static String validateFedIdRequest(String email, String iamUserId, String tenantId){
-        if(iamUserId == null && email == null){
-            return "email or iamuserid";
-        }else if(tenantId  == null){
+    /**
+     *
+     * @param fedIdRequestBean
+     *
+     * Validate FedIdRequestBean
+     * email, tenantId, status and role are mandatory fields
+     *
+     * @return
+     */
+    public static String validateFedIdRequestBean(FedIdRequestBean fedIdRequestBean) {
+        if (fedIdRequestBean.getEmail() == null || fedIdRequestBean
+          .getEmail()
+          .trim()
+          .isEmpty()) {
+            return "email";
+        } else if (!EmailValidator
+          .getInstance()
+          .isValid(fedIdRequestBean.getEmail())) {
+            return "email";
+        }
+        if (fedIdRequestBean.getTenantId() == null || fedIdRequestBean
+          .getTenantId()
+          .trim()
+          .isEmpty()) {
             return "tenantId";
         }
-        else return null;
+        if (fedIdRequestBean.getStatus() == null || fedIdRequestBean
+          .getStatus()
+          .trim()
+          .isEmpty()) {
+            return "status";
+        }
+        if (fedIdRequestBean.getRole() == null || fedIdRequestBean
+          .getRole()
+          .trim()
+          .isEmpty()) {
+            return "role";
+        }
+        return null;
+    }
+
+    public static String validateFedIdRequest(String email, String iamUserId, String tenantId) {
+        if (iamUserId != null) if (iamUserId.isEmpty() == false) return null;
+        if (email != null) if (email.isEmpty() == false) return null;
+        if (iamUserId == null && email == null) {
+            return "email or iamuserid";
+        } else if (tenantId == null) {
+            return "tenantId";
+        } else return null;
     }
 }
 
--- authenticateb/trunk/src/test/java/com/tmobile/federation/services/CreateFedIdServiceTest.java
+++ authenticateb/trunk/src/test/java/com/tmobile/federation/services/CreateFedIdServiceTest.java
@@ -1,41 +1,28 @@
 package com.tmobile.federation.services;
 
-import com.tmobile.federation.beans.req.FedIdRequestBean;
-import com.tmobile.federation.beans.req.userProfile.UserProfiles;
-import com.tmobile.federation.beans.res.GlobalErrorBean;
-import com.tmobile.federation.beans.res.GlobalResponseBean;
 import com.tmobile.federation.dao.IAMUserDAO;
 import com.tmobile.federation.dao.IAMUserDAOImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.inject.Inject;
 
 import java.util.Arrays;
 
-import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -47,8 +34,7 @@ public class CreateFedIdServiceTest {
 
     private MockMvc mockMvc;
 
-    @InjectMocks
-    private CreateFedIdservice createFedIdservice;
+    @InjectMocks private CreateFedIdservice createFedIdservice;
 
     @Before
     public void setUp() {
@@ -60,23 +46,85 @@ public class CreateFedIdServiceTest {
     }
 
     @Test
-    public void testGetFedIDService() throws Exception {
+    public void mockQueryAnExistedFederatedIDFromWebLogicThen200OK() throws Exception {
         IAMUserDAO iamUserDAO = new IAMUserDAOImpl();
         createFedIdservice.setIAMUserDAO(iamUserDAO);
         ResultActions resultActions = mockMvc
           .perform(get("/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&email=email@mobile.com&tenantId=xyz123")
             .contentType("application/json")
             .header("authorization", "admin:123123")
-            .header("transactionId", "123123")
-          )
-          .andExpect(status().isOk());
-        System.out.println("passed xxxx");
+            .header("transactionId", "123123"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value() + " " + HttpStatus.OK.getReasonPhrase()))
+          .andExpect(jsonPath("$.object.role").value("SuperAdmin"));
         resultActions.andDo(MockMvcResultHandlers.print());
 
     }
 
     @Test
-    public void queryFederatedIDFromWebLogic() {
+    public void mockQueryMissingEmailAnExistedFederatedIDThen200OK() throws Exception {
+        IAMUserDAO iamUserDAO = new IAMUserDAOImpl();
+        createFedIdservice.setIAMUserDAO(iamUserDAO);
+        ResultActions resultActions = mockMvc
+          .perform(get("/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&tenantId=xyz123")
+            .contentType("application/json")
+            .header("authorization", "admin:123123")
+            .header("transactionId", "123123"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value() + " " + HttpStatus.OK.getReasonPhrase()))
+          .andExpect(jsonPath("$.object.role").value("SuperAdmin"));
+        resultActions.andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void mockQueryMissingTenantIDAnExistedFederatedIDThen200OK() throws Exception {
+        IAMUserDAO iamUserDAO = new IAMUserDAOImpl();
+        createFedIdservice.setIAMUserDAO(iamUserDAO);
+        ResultActions resultActions = mockMvc
+          .perform(get("/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&email=email@mobile.com")
+            .contentType("application/json")
+            .header("authorization", "admin:123123")
+            .header("transactionId", "123123"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value() + " " + HttpStatus.OK.getReasonPhrase()))
+          .andExpect(jsonPath("$.object.role").value("SuperAdmin"));
+        resultActions.andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void mockQueryMissingEmailAndTenantIDAnExistedFederatedIDThen200OK() throws Exception {
+        IAMUserDAO iamUserDAO = new IAMUserDAOImpl();
+        createFedIdservice.setIAMUserDAO(iamUserDAO);
+        ResultActions resultActions = mockMvc
+          .perform(get("/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3")
+            .contentType("application/json")
+            .header("authorization", "admin:123123")
+            .header("transactionId", "123123"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value() + " " + HttpStatus.OK.getReasonPhrase()))
+          .andExpect(jsonPath("$.object.role").value("SuperAdmin"));
+        resultActions.andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void mockQueryANonExistedFederatedIDThen400BadRequest() throws Exception {
+        IAMUserDAO iamUserDAO = new IAMUserDAOImpl();
+        createFedIdservice.setIAMUserDAO(iamUserDAO);
+        ResultActions resultActions = mockMvc
+          .perform(get("/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9&email=emai@mobile.com&tenantId=xyz23")
+            .contentType("application/json")
+            .header("authorization", "admin:123123")
+            .header("transactionId", "123123"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.statusCode").value(HttpStatus.BAD_REQUEST.value() + " " + HttpStatus.BAD_REQUEST.getReasonPhrase()))
+          .andExpect(jsonPath("$.object").value("User Does Not Exists"));
+    }
+
+    @Test
+    public void clientQueryAnExistedFederatedIDFromWebLogicThen200OK() {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -89,11 +137,161 @@ public class CreateFedIdServiceTest {
         String responseBody = responseEntity.getBody();
         System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
         assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
-        assertThat(responseBody,containsString("role"));
-        assertThat(responseBody,
-          hasJsonPath("role", containsString("SuperAdmin")));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
     }
 
+    @Test
+    public void clientQueryMissingIAMUserIDAnExistedFederatedIDFromWebLogicThen200OK() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?email=email@mobile.com&tenantId=xyz123";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
 
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
+    }
+
+    @Test
+    public void clientQueryMissingIAMUserIDAndTenantIDAnExistedFederatedIDFromWebLogicThen200OK() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?email=email@mobile.com";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
+    }
+
+    @Test
+    public void clientQueryMissingEmailAnExistedFederatedIDFromWebLogicThen200OK() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&tenantId=xyz123";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
+    }
+
+    @Test
+    public void clientQueryMissingTenantAnExistedFederatedIDFromWebLogicThen200OK() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3&email=email@mobile.com";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
+    }
+
+    @Test
+    public void clientQueryMissingEmailAndTenantAnExistedFederatedIDFromWebLogicThen200OK() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9fb3";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("role"));
+        assertThat(responseBody, containsString("SuperAdmin"));
+    }
+
+    @Test
+    public void clientQueryMissingAllFederatedIDFromWebLogicThen400BadRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("400 Bad Request"));
+        assertThat(responseBody, containsString("Missing mandatory parameter"));
+    }
+
+    @Test
+    public void clientQueryANonExistedFederatedIDFromWebLogicThen400BadRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("authorization", "admin:123123");
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9&email=email1@mobile.com&tenantId=xyz1231";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("400 Bad Request"));
+        assertThat(responseBody, containsString("User Does Not Exists"));
+    }
+
+    @Test
+    public void clientQueryMissAuthorizationHeaderFederatedIDFromWebLogicThen400BadRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("transactionId", "123123");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9&email=email1@mobile.com&tenantId=xyz1231";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("401 Unauthorized"));
+    }
+
+    @Test
+    public void clientQueryMissHeaderFederatedIDFromWebLogicThen400BadRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String queryFederatedID = "http://10.30.164.132:7001/authenticateb/provisioning/v1/fedId?iamUserId=U-82bf6ba6-88f5-4049-b86c-860dca0c9&email=email1@mobile.com&tenantId=xyz1231";
+        ResponseEntity<String> responseEntity = restTemplate.exchange(queryFederatedID, HttpMethod.GET, entity, String.class);
+        String responseBody = responseEntity.getBody();
+        System.out.println("responseEntity UserProfiles from Server .... \n" + responseBody);
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(responseBody, containsString("401 Unauthorized"));
+    }
 
 }
